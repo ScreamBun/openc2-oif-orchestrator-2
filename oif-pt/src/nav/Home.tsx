db8001edafe70690b79fe5a45c791e2e75ae8711
Kevin Cressman <kgcressman@praxiseng.com>
2024-03-20 15:05:21 -0400

    cleaned up home page

--- oif-pt/src/nav/Home.tsx
+++ oif-pt/src/nav/Home.tsx
@@ -18,7 +18,7 @@ const Home = () => {
                                 the &quot;Device&quot; which functions as an OpenC2 consumer.
                             </p>
                             <p>
-                                This application is the OpenC2 Orchestrator. The Device repository can be found&nbsp;
+                                This application is an OpenC2 Orchestrator. The Device repository can be found&nbsp;
                                 <a href={NAV_EXTERNAL_OIF_DEVICE} rel="noopener noreferrer" target="_blank">here</a>
                                 .&nbsp;Due to port bindings it is recommended that the orchestrator and the device not be run on the same
                                 machine.
@@ -38,11 +38,8 @@ const Home = () => {
                         <div className='card-header'>Transports</div>
                         <div className='card-body p-0'>
                             <ul className="list-group">
-                                <li className="list-group-item disabled"> CoAP - Nonstandard, no specification</li>
-                                <a className="list-group-item list-group-item-action" href={NAV_EXTERNAL_HTTPS} rel="noopener noreferrer" target="_blank">HTTPS<sup className='text-muted'>* Official</sup></a>
-                                <a className="list-group-item list-group-item-action" href={NAV_EXTERNAL_MQTT} rel="noopener noreferrer" target="_blank">MQTT - In Progress</a>
-                                <a className="list-group-item list-group-item-action" href={NAV_EXTERNAL_OPENDXL} rel="noopener noreferrer" target="_blank">OpenDXL - In Progress</a>
-                                <li className="list-group-item disabled"> ZMQ - Nonstandard, no specification</li>
+                                <a className="list-group-item list-group-item-action" href={NAV_EXTERNAL_HTTPS} rel="noopener noreferrer" target="_blank">HTTPS</a>
+                                <a className="list-group-item list-group-item-action" href={NAV_EXTERNAL_MQTT} rel="noopener noreferrer" target="_blank">MQTT</a>
                             </ul>
                         </div>
                     </div>
@@ -52,23 +49,8 @@ const Home = () => {
                         <div className='card-header'>Serializations</div>
                         <div className='card-body p-0'>
                             <ul className="list-group">
-                                <a className="list-group-item list-group-item-action" href={NAV_EXTERNAL_BINN} rel="noopener noreferrer" target="_blank">Binn</a>
-                                <a className="list-group-item list-group-item-action" href={NAV_EXTERNAL_BENCODE} rel="noopener noreferrer" target="_blank">Bencode</a>
-                                <a className="list-group-item list-group-item-action" href={NAV_EXTERNAL_BSON} rel="noopener noreferrer" target="_blank">BSON</a>
-                                <a className="list-group-item list-group-item-action" href={NAV_EXTERNAL_CBOR} rel="noopener noreferrer" target="_blank">CBOR</a>
-                                <a className="list-group-item list-group-item-action" href={NAV_EXTERNAL_ION} rel="noopener noreferrer" target="_blank">Amazon ION</a>
                                 <a className="list-group-item list-group-item-action" href={NAV_EXTERNAL_JSON} rel="noopener noreferrer" target="_blank">JSON<sup className='text-muted'>* Official</sup> </a>
-                                <a className="list-group-item list-group-item-action" href={NAV_EXTERNAL_MSGPACK} rel="noopener noreferrer" target="_blank">MessagePack (msgpack)</a>
-                                <a className="list-group-item list-group-item-action" href={NAV_EXTERNAL_SEXP} rel="noopener noreferrer" target="_blank">S-expressions</a>
-                                <a className="list-group-item list-group-item-action" href={NAV_EXTERNAL_SMILE} rel="noopener noreferrer" target="_blank">Smile</a>
-                                <a className="list-group-item list-group-item-action" href={NAV_EXTERNAL_TOML} rel="noopener noreferrer" target="_blank">Toml</a>
                                 <a className="list-group-item list-group-item-action" href={NAV_EXTERNAL_XML} rel="noopener noreferrer" target="_blank">XML</a>
-                                <a className="list-group-item list-group-item-action" href={NAV_EXTERNAL_UBJSON} rel="noopener noreferrer" target="_blank">UBJSON</a>
-                                <a className="list-group-item list-group-item-action" href={NAV_EXTERNAL_VPACK} rel="noopener noreferrer" target="_blank">
-                                    VelocityPack (VPack)
-                                    <p className="mb-1 small text-muted">- Requires velocity pack to be installed, only C++ module available</p>
-                                </a>
-                                <a className="list-group-item list-group-item-action" href={NAV_EXTERNAL_YAML} rel="noopener noreferrer" target="_blank">YAML</a>
                             </ul>
                         </div>
                     </div>
